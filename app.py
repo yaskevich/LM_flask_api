182ba0fd834d1530c8e27f791ad04a853bd97294
yaskevich <alyaxey.yaskevich@gmail.com>
2019-12-27 23:19:31 +0300

    permanent model load

--- app.py
+++ app.py
@@ -7,24 +7,25 @@ app = Flask(__name__)
 api = Api(app)
 
 todos = {}
+HMM = myHMM()
 
 class Predictability(Resource):
     def get(self):
-        sentence  = request.form['data']
+        # sentence  = request.form['data']  or 'I like to eat apples'
+        sentence  = 'I like to eat apples'
         Bert = myBert()
         result = Bert.get_probabilities(sentence)
         return jsonify(sentence = result)
 
 class HMMPredict(Resource):
     def get(self):
-        words = request.form['data']
-        HMM = myHMM()
+        words = '' # request.form['data']
         result = HMM.get_words(words)
         return jsonify(sentence = result)
 
 
 api.add_resource(Predictability, '/')
-api.add_resource(HMMPredict, '/HMM')
+api.add_resource(HMMPredict, '/predict')
 
 if __name__ == '__main__':
     app.run(debug=True)
